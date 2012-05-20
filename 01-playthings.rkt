4312c8a266b769077fe065fb68d6b1a0e8022c1b
rod <rod@pu-gh.com>
2012-05-20 16:57:38 +0100

    answers up to 40

--- 01-playthings.rkt
+++ 01-playthings.rkt
@@ -26,3 +26,103 @@
       (== #t x)
       (== #t q))))
 
+; 26
+
+(ans '(#t)
+  (run* (q)
+    (fresh (x)
+      (== x #t)
+      (== #t q))))
+
+; 27
+
+(ans '(#t)
+  (run* (q)
+    (fresh (x)
+      (== x #t)
+      (== q #t))))
+
+; 29
+
+(ans '(_.0)
+  (run* (x)
+    (let ((x #f))
+      (fresh (x)
+        (== #t x)))))
+
+; 30
+
+(ans '((_.0 _.1))
+  (run* (r)
+    (fresh (x y)
+      (== (cons x (cons y '())) r))))
+
+; 31
+
+(ans '((_.0 _.1))
+  (run* (s)
+    (fresh (t u)
+      (== (cons t (cons u '())) s))))
+
+; 32
+
+(ans '((_.0 _.1 _.0))
+  (run* (r)
+    (fresh (x)
+      (let ((y x))
+        (fresh (x)
+          (== (cons y (cons x (cons y '()))) r))))))
+
+; 33
+
+(ans '((_.0 _.1 _.0))
+  (run* (r)
+    (fresh (x)
+      (let ((y x))
+        (fresh (x)
+          (== (cons x (cons y (cons x '()))) r))))))
+
+; 34
+
+(ans '()
+  (run* (q)
+    (== #f q)
+    (== #t q)))
+
+; 35
+
+(ans '(#f)
+  (run* (q)
+    (== #f q)
+    (== #f q)))
+
+; 36
+
+(ans '(#t)
+  (run* (q)
+    (let ((x q))
+      (== #t x))))
+
+; 37
+
+(ans '(_.0)
+  (run* (r)
+    (fresh (x)
+      (== x r))))
+
+; 38
+
+(ans '(#t)
+  (run* (q)
+    (fresh (x)
+      (== #t x)
+      (== x q))))
+
+; 39
+
+(ans '(#t)
+  (run* (q)
+    (fresh (x)
+      (== x q)
+      (== #t x))))
+
