0eaf6b686f0125b5cd224bd9ea8b129c7a2b8306
rod <rhodri.pugh@boxuk.com>
2012-05-22 21:06:33 +0100

    Answers to 56

--- 01-playthings.rkt
+++ 01-playthings.rkt
@@ -154,3 +154,66 @@
       ((== 'oil x) S)
       (U))))
 
+; 49
+
+(ans '(olive)
+  (run 1 (x)
+    (conde
+      ((== 'olive x) S)
+      ((== 'oil x) S)
+      (U))))
+
+; 50
+
+(ans '(olive _.0 oil)
+  (run* (x)
+    (conde
+      ((== 'virgin x) U)
+      ((== 'olive x) S)
+      (S S)
+      ((== 'oil x) S)
+      (U))))
+
+; 52
+
+(ans '(extra olive)
+  (run 2 (x)
+    (conde
+      ((== 'extra x) S)
+      ((== 'virgin x) U)
+      ((== 'olive x) S)
+      ((== 'oil x) S))))
+
+; 53
+
+(ans '((split pea))
+  (run* (r)
+    (fresh (x y)
+      (== 'split x)
+      (== 'pea y)
+      (== (cons x (cons y '())) r))))
+
+; 54
+
+(ans '((split pea soup) (navy bean soup))
+  (run* (r)
+    (fresh (x y)
+      (conde
+        ((== 'split x) (== 'pea y))
+        ((== 'navy x) (== 'bean y))
+        (U))
+      (== (cons x (cons y (cons 'soup '()))) r))))
+
+; 56
+
+(define teacupo
+  (lambda (x)
+    (conde
+      ((== 'tea x) S)
+      ((== 'cup x) S)
+      (U))))
+
+(ans '(tea cup)
+  (run* (x)
+    (teacupo x)))
+
